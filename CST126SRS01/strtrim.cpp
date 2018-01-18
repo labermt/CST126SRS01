0652a3f4a7fc42d654b6fd24a96c67a6ca9e5835
seadra <colin.rexing@oit.edu>
2018-01-17 21:29:03 -0800

    First run at assignment, created strtrim. Working on start of string to check for whitespace.

---
+++ CST126SRS01/strtrim.cpp
@@ -0,0 +1,25 @@
+
+#include "stdafx.h"
+#include <cctype>  
+#include <cstddef>  
+#include <cstring> 
+
+char* strtrim(char str[])
+{
+	char* begin = str;
+	char* end = nullptr;
+
+	if (str == nullptr)
+	{
+		return end;
+	}
+	else
+	{
+		while (end != '\0' || begin != '\0')
+		{
+			
+		}
+	}
+
+
+}
