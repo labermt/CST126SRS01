0c5f5cbabdac0babb8ca018e64a691354acd9025
TheJonBovi <35358211+TheJonBovi@users.noreply.github.com>
2018-01-19 19:01:28 -0800

    worked on strtrim
    made pointers to start and end of file and brought them to first/last characters of actual text.

--- CST126SRS01/strtrim.cpp
+++ CST126SRS01/strtrim.cpp
@@ -1,9 +1,32 @@
 
 #include "stdafx.h"
 #include "strtrim.h"
+#include <ctype.h>
+#include <string>
 
 char* strtrim(char str[])
 {
+	//Pointer to beginning of str
+	char* firstChar = &str[0];
+
+	size_t size = sizeof(str);
+
+	//Pointer to end of string
+	char* lastChar = &str[size];
+
+	//Does \t take up one char in memory?
+	if (isspace(*firstChar)) firstChar++;
+	if (isspace(*lastChar)) lastChar--;
+
+	//Lenght of trimmed string, plus one for addition of null char
+	int lenghtOfTrimmed = lastChar - firstChar + 1;
+
+	//how do I make this new array without memory leak? I would have to add it to the heap but couldn't get rid of it until later in main.cpp :(
+
+
+
+
+
 	return 0; //This has to return a pointer to the new string that has been trimmed.
 
 }
