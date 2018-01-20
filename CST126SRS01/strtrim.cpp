cfdda5c84a232d9556d42b50763c56703b8b2fd9
seadra <colin.rexing@oit.edu>
2018-01-19 18:52:00 -0800

    Getting closer

--- CST126SRS01/strtrim.cpp
+++ CST126SRS01/strtrim.cpp
@@ -10,7 +10,7 @@ char* strtrim(char str[])
 {
 	char* begin = str;
 	char* scan = str;
-	//char* end = nullptr;
+	char* end = nullptr;
 
 	if (str == nullptr || *str == '\0')
 	{
@@ -20,31 +20,36 @@ char* strtrim(char str[])
 	{
 		while (isspace(*scan) && *scan != '\0')
 		{
-			//*scan = '\0';
+			//posible solution *begin = scan* increment both until \0 then back fill with \0 until non white space
 			++scan;
 		}
-
 		begin = scan;
-
 		while (*scan != '\0')
 		{
 			++scan;
 		}
+		end = scan;
 
-		if (scan == begin)
+		if (begin == end)
 		{
-		 // intentionally blank
+			str[0] = '\0';
 		}
 		else
 		{
+			while (begin != end)
+			{
+				int i = 0;
+				str[i] = begin[i];
+				++i;
+				++begin;
+			}
 			--scan;
 			while (isspace(*scan) || *scan == '\0')
-			{	
+			{
 				*scan = '\0';
 				--scan;
 			}
 		}
-		str = begin;
 	}
 	return str;
 }
