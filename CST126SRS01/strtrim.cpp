ca9544081b398d727051d85e6e7316c51fcc28fa
TheJonBovi <deraitaa@gmail.com>
2018-01-20 14:07:09 -0800

    strtrim now works for all test cases
    some shoring up still to be done however

--- CST126SRS01/strtrim.cpp
+++ CST126SRS01/strtrim.cpp
@@ -6,33 +6,29 @@
 
 char* strtrim(char str[])
 {
-
-	char* const result = str;
-
 	if (str == nullptr || *str == '\0') return str;
 
 	//Pointer to beginning of str
 	const char* firstChar = str;
 
-	// not sure why this isnt working...
-	const size_t size = strlen(str) + 1;
-
 	//Pointer to end of string
-	char* lastChar = &str[size - 1];
+	char* lastChar = strchr(str, '\0') - 1;
+
+	size_t size = strlen(str);
+
+	while (isspace(*lastChar))
+	{
+		if (firstChar == lastChar) break;
+		lastChar--;
+	}
+	*(lastChar + 1) = '\0';
+
+	size = strlen(str);
 
 	while (isspace(*firstChar)) firstChar++;
 
-	memmove(str, firstChar, size);
+	memmove(str, firstChar, size + 1);
 
-	if (size > 0)
-	{
-		while (isspace(*lastChar)) lastChar--;
-		*(lastChar + 1) = '\0';
-	}
-	else if (size == 1)
-	{
-		*(lastChar) = '\0';
-	}
 
 	return str; //This has to return a pointer to the new string that has been trimmed.
 
