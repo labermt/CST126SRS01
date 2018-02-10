6ac433976b1e95cdef9a70150829eb1ea22a3781
Mitch Laber <labermt@gmail.com>
2018-02-09 21:01:26 -0800

    Removed memory violations and dead code.

--- CST126SRS01/strtrim.cpp
+++ CST126SRS01/strtrim.cpp
@@ -1,52 +1,46 @@
 #include "stdafx.h" // Must be first in this file.
 #include <cstring>
 #include <cassert>
-#include "strtrim.h"
-#include <cstdio>
-#include <ctype.h>
+#include <cctype>
 #include <iostream>
+#include "strtrim.h"
 
 
 char* strtrim(char str[])
 {
-
 	char* begin = str;
 
 	if (str != nullptr && *str != '\0')
 	{
-
 		while (isspace(*begin))
 		{
 			++begin;
 		}
-		//std::cout << begin;
+
 		char* charFirst = begin;
 		char* foundChar = begin;
 
 		while (*foundChar != '\0')
 		{
 			++foundChar;
-
 		}
 
 		char* foundcNull = foundChar - 1;
 		char* cpFcNull = foundChar;
 
-		
-		while (isspace(*foundcNull))
-		{
-			if (foundcNull == str)
-				foundcNull - 1; 
-				break;
-			foundcNull--;
-			
-		}
+		//while (isspace(*foundcNull)) // Dead code. 
+		//{
+		//	if (foundcNull == str)
+		//		foundcNull - 1;
+		//		break;
+		//	foundcNull--;
+		//}
 		size_t size = strlen(str);
 
-		memmove(foundcNull + 1, cpFcNull, size);
+		memmove(foundcNull + 1, cpFcNull, size); // Memory violation. Reading beyond the end of the string. 
 		memmove(str, charFirst, size + 1);
 
-		std::cout << str;
+		// TODO: Remove scaffolding: std::cout << str;
 
 	}
 	return str;
